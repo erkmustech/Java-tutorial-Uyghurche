2234355b925d01171eb12eafb3b1ae5a0a050b93
erk.tech <erk.mustech@gmail.com>
2020-09-09 22:34:04 +0200

    day26uploaded

---
+++ src/day26_array/Array_2D.java
@@ -0,0 +1,19 @@
+package day26_array;
+
+import java.util.Scanner;
+
+public class Array_2D {
+public static void main(String[] args) {
+  
+  int[] table=new int[3];
+  int[][] arrays=new int[3][4];
+  System.out.println("inter your name");
+  Scanner s=new Scanner(System.in);
+  String name=s.nextLine();
+  System.out.println(" please inter you surname");
+  String surname=s.nextLine();
+  System.out.println("your name is "+name+"  "+surname);
+
+}
+  
+}
---
+++ src/day26_array/Two_dimensional_array.java
@@ -0,0 +1,10 @@
+package day26_array;
+
+public class Two_dimensional_array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

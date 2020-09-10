83515f8f717e290eb72fb5d2b2d81feb97540be6
erk.tech <erk.mustech@gmail.com>
2020-09-10 22:46:15 +0200

    day27

---
+++ src/day27_Array_part4/Teams.java
@@ -0,0 +1,24 @@
+package day27_Array_part4;
+
+public class Teams {
+public static void main(String[] args) {
+  
+  Sting[][] teams=new Strig [2][4];
+  team[0][1]="Tony";
+  team[0][2]="Joen";
+  team[0][3]="Biden";
+  team[0][4]="Erk";
+
+    team[1][1]="Ruan";
+    team[1][2]="Furkan";
+    team[2][3]="Max";
+    team[1][4]="Min";
+
+    System.out.println("first player of the first team :");
+    System.out.println(team[0][1]);
+
+
+
+}
+  
+}
